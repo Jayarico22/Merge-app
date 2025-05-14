a53c6c7a9b513922827805b4c5a18280d2912c8e
Jayarico22 <150088622+Jayarico22@users.noreply.github.com>
2025-05-14 03:23:59 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('MERGE Backend')); app.listen(3001);
+const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('MERGE Backend Running')); app.listen(4000);
